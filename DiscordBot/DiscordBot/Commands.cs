da97efae1d2528b7782b6d40238b0083c2dad9cd
AsienPanda <asienpanda@gmail.com>
2016-04-15 21:18:52 -0700

    Created Dice and Choose commands.

--- DiscordBot/DiscordBot/Commands.cs
+++ DiscordBot/DiscordBot/Commands.cs
@@ -20,8 +20,98 @@ namespace DiscordBot
             .Do(async e =>
             {
                 await e.Channel.SendMessage(e.User.Name + " greets " + e.GetArg("GreetedPerson"));
-            //sends a message to channel with the given text
-        });
+                //sends a message to channel with the given text
+            });
+
+            commands.CreateCommand("roll")
+            .Alias(new string[] { "dice" })
+            .Description("Rolls dice.")
+            .Parameter("NumberOfDice", ParameterType.Required)
+            .Parameter("NumberOfSides", ParameterType.Required)
+            .Parameter("AddFlag", ParameterType.Optional)
+            .Do(async e =>
+             {
+                 int numDice = Int32.Parse(e.GetArg("NumberOfDice"));
+                 int numSides = Int32.Parse(e.GetArg("NumberOfSides"));
+
+                 if (numDice > 100)
+                 {
+                     await e.Channel.SendMessage("_You cannot roll more than 100 dice!_");
+                 }
+                 else if (numSides > 999)
+                 {
+                     await e.Channel.SendMessage("_Dice cannot have more than 999 sides!_");
+                 }
+                 else {
+                     int sum = 0;
+                     int[] array = new int[numDice];
+
+                     Random rnd = new Random();
+                     for (int i = 0; i < numDice; ++i)
+                     {
+                         array[i] = rnd.Next(0, numSides) + 1;
+                         sum += array[i];
+
+                     }
+                     String msg = String.Format("{0}{1}", "```", array[0]);
+                     for (int i = 1; i < numDice; ++i)
+                     {
+                         msg = String.Format("{0}, {1}", msg, array[i]);
+                     }
+                     msg = msg + "```";
+                     await e.Channel.SendMessage(msg);
+                     if (Boolean.Parse(e.GetArg("AddFlag")))
+                     {
+                         await e.Channel.SendMessage(String.Format("_The sum is: {0}_", sum));
+                     }
+                 }
+             });
+            commands.CreateCommand("choose")
+                .Alias(new string[] { "erabe" })
+                .Description("Randomly decides a choice. Multi-word answers must be used in \"quotation marks\"!")
+                .Parameter("option", ParameterType.Required)
+                .Parameter("option", ParameterType.Required)
+                .Parameter("option", ParameterType.Multiple)
+                .Do(async e =>
+                {
+                    //redefine parameters
+                    List<String> list = new List<String>();
+                    for (int i = 0; i < e.Args.Length; ++i)
+                    {
+
+                        if (e.GetArg(i).Contains("\""))
+                        {
+                            ++i;
+                            String s = "";
+                            while (e.GetArg(i) != "\"" && i < e.Args.Length - 1)
+                            {
+                                s = s + e.GetArg(i) + " ";
+                                ++i;
+                            }
+
+                            if (e.GetArg(i).Contains("\""))
+                            {
+                                list.Add(s);
+                            }
+                        }
+                        else {
+                            list.Add(e.GetArg(i));
+                        }
+
+                    }
+                    if (list.Count < 2)
+                    {
+                        await e.Channel.SendMessage("_You must enter at least 2 options!_");
+                    }
+                    else {
+                        Random rnd = new Random();
+
+                        await e.Channel.SendMessage(String.Format("_Misaka chooses **{0}**_", list.ElementAt(rnd.Next(0, list.Count - 1))));
+                    }
+
+
+                });
+
             //Example of command group
             //we would run our commands with ~do greet X and ~do bye X
             commands.CreateGroup("do", cgb =>
--- DiscordBot/DiscordBot/Properties/AssemblyInfo.cs
+++ DiscordBot/DiscordBot/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+// by using the '_' as shown below:
+// [assembly: AssemblyVersion("1.0._")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
